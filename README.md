ae36c1e891403eba4cce8bdecaea9b5b89de892d
prateek <prateek@ve.proxmox.local>
2024-01-27 00:57:02 +0530

    shell.nix

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Eye-disease-classification
+
+to execute install nix from [https://nixos.org/](https://nixos.org/)
+
+using command 
+`sh <(curl -L https://nixos.org/nix/install) --daemon`
+
+
+and run with 
+
+`nix-shell`
--- shell.nix
+++ shell.nix
@@ -3,30 +3,30 @@
 
 pkgs.mkShell {
   name="kivy-dev";
-  buildInputs = [
-    pkgs.pipenv
-	pkgs.vscodium
-	pkgs.python311
-    pkgs.python311Packages.pywebview
-	pkgs.qt5.qtwebengine
-	pkgs.python311Packages.virtualenv
-	pkgs.python311Packages.cython
-	pkgs.python311Packages.tkinter
-	pkgs.python311Packages.kivy
-	pkgs.python311Packages.openpyxl
-	pkgs.python311Packages.pyautogui
-	pkgs.python311Packages.beautifulsoup4
-	pkgs.python311Packages.requests
-	pkgs.python311Packages.numpy
-	pkgs.python311Packages.tensorflow
-	pkgs.python311Packages.keras
-	pkgs.python311Packages.matplotlib
-	pkgs.python311Packages.pandas
-	pkgs.python311Packages.jupyterlab
-	pkgs.python311Packages.opencv4
-	pkgs.python311Packages.seaborn
-	pkgs.python311Packages.tqdm
-	pkgs.python311Packages.sklearn-deap
+  buildInputs = with pkgs; [
+    	pipenv
+	vscodium
+	qt5.qtwebengine
+	python311
+    	python311Packages.pywebview
+	python311Packages.virtualenv
+	python311Packages.cython
+	python311Packages.tkinter
+	python311Packages.kivy
+	python311Packages.openpyxl
+	python311Packages.pyautogui
+	python311Packages.beautifulsoup4
+	python311Packages.requests
+	python311Packages.numpy
+	python311Packages.tensorflow
+	python311Packages.keras
+	python311Packages.matplotlib
+	python311Packages.pandas
+	python311Packages.jupyterlab
+	python311Packages.opencv4
+	python311Packages.seaborn
+	python311Packages.tqdm
+	python311Packages.sklearn-deap
   ];
   
   shellHook = ''
